42d673f0d076c96e884fbd10dc57aa3f999ed9d7
David Moravec <david.moravec2@seznam.cz>
2024-10-03 17:06:07 +0200

    prvni pouzivani useefect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from "react";
+
+const App = () => {
+
+  const [quote, setQuote] = useState("")
+
+  const url = "https://api.kanye.rest"
+
+  const getQuote = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    const finalQuote = data["quote"]
+    setQuote(finalQuote)
+  }
+
+
+  useEffect(() => {
+    getQuote()
+  }, [])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="kanye">
+      <h1>Kanye West said: </h1>
+      <h2>{quote}</h2>
+      <button className="btn" onClick={getQuote}>Another one</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
